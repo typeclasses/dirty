b65ab9d15ab023759fddbf412396e38ed2272968
Julie Moronuki <jdog74@gmail.com>
2018-03-24 02:30:16 -0400

    add option flags but still not dirty

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Turtle
 
+parser :: Parser (Text, Int)
+parser = (,) <$> optText "name" 'n' "Your first name"
+             <*> optInt  "age"  'a' "Your current age"
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    (name, age) <- options "Greeting script" parser
+    echo (repr (format ("Hello there, "%s) name))
+    echo (repr (format ("You are "%d%" years old") age))
+
